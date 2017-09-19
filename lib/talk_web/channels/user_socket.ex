41eebc4ee3f3e432be651c2c706c04e65035b208
Robert Cramer <robert.cramer@fivebox.com>
2017-09-18 22:37:46 -0400

    prepare websockets for production

--- lib/talk_web/channels/user_socket.ex
+++ lib/talk_web/channels/user_socket.ex
@@ -7,7 +7,8 @@ defmodule TalkWeb.UserSocket do
   channel "slide:lobby", TalkWeb.SlideChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
